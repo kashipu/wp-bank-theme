0cb9255f6e298c93aedd1573d13f3da754bdcb0f
kashipu <graficowm@gmail.com>
2021-09-19 23:17:01 -0500

    Ajustes y página contactenos

--- app/public/wp-content/themes/temaTest/page.php
+++ app/public/wp-content/themes/temaTest/page.php
@@ -1,5 +1,5 @@
 <?php get_header() ?>
-<div class="page__container" style="margin-top:108px">
+<div class="page__container" style="margin-top:70px">
     <div class="page__hero" style="background-image: url(<?php echo get_the_post_thumbnail_url() ?>)">
         <h1 class="page__title"><?php the_title(); ?></h1>
     </div>
